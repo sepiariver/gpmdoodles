4d7eb3e51ec0cf2faa8b4fcfaadd565bda33714d
sepiariver <yj@paywith.com>
2020-04-19 09:43:39 -0700

    some core stuff

---
+++ core/components/gpm-doodles/model/src/GPMDoodles.php
@@ -0,0 +1,15 @@
+<?php 
+namespace SepiaRiver\GPMDoodles;
+use GuzzleHttp\Client;
+use \modX;
+use \xPDO;
+use \PDO;
+
+/**
+ * The main MRAdmin service class.
+ *
+ * @package mradmin
+ */
+class GPMDoodles {
+    
+}
